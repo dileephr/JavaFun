aaa2fc3daaaf8408bd8cccdfc74df89c1670ed8a
Dileep Rao <dileep@Dileeps-MacBook-Pro.local>
2014-07-22 22:41:50 -0700

    Second commit

---
+++ JavaFun/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  JavaFun
+//
+//  Created by Dileep Rao on 7/22/14.
+//  Copyright (c) 2014 Dileep Rao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
